d440c10e1737ef0b7a6ba9597da72811549fd22d
rvpout <rvpout@utu.fi>
2021-01-14 11:30:40 +0200

    update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,6 +11,7 @@ function HeroSection() {
         <div className='hero-container'>
             <h1>Welcome To My Portfolio</h1>
             <p>I am computer science student in University of Turku</p>
+            
             <div className="hero-btns">
                 <Button 
                 className='btns' 
